4617ea611c3cb0da399e155e8769e59e9ae1f9e4
Stephen Atehe <atehestephen@gmail.com>
2022-01-27 14:48:41 +0100

    updated makedirs.py to create folders in snake case

--- makedirs.py
+++ makedirs.py
@@ -1,5 +1,3 @@
-
-
 import os
 from pathlib import Path
 
@@ -7,7 +5,7 @@ cwd = Path.cwd()
 
 for i in range(12):
     folders = os.listdir(cwd)
-    new_folder = 'chapter ' + str(i)
+    new_folder = "chapter_" + str(i)
     if not new_folder in folders:
-        print(cwd / new_folder)
+        print(f"Creating {cwd }/{new_folder}")
         # os.makedirs(cwd / new_folder)
