bb7a35e16de98b0fea1decb9d0d467dace3fccb6
Stephen Atehe <atehestephen@gmail.com>
2022-01-27 14:51:00 +0100

    uncommented os.mkdir() command

--- makedirs.py
+++ makedirs.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 cwd = Path.cwd()
 
-for i in range(12):
+for i in range(15):
     folders = os.listdir(cwd)
     new_folder = "chapter_" + str(i)
     if not new_folder in folders:
         print(f"Creating {cwd }/{new_folder}")
-        # os.makedirs(cwd / new_folder)
+        os.makedirs(cwd / new_folder)
