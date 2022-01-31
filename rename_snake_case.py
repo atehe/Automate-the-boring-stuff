85bd37e855cec9cd814f589c1a1fbc6c31242d79
Stephen Atehe <atehestephen@gmail.com>
2022-01-31 20:53:46 +0100

    refactor code

--- rename_snake_case.py
+++ rename_snake_case.py
@@ -1,8 +1,7 @@
 #! usr/bin/python3
-# script to rename files and folder in directory to snake case
+# rename_snake_case.py - rename files and folder in directory to snake case
 
 import os
-from pathlib import Path
 
 
 def rename_snake_case(path):
@@ -15,16 +14,18 @@ def rename_snake_case(path):
             old_filepath = os.path.join(folder, file)
             new_filepath = os.path.join(folder, file_snakecase)
 
-            print(f"renaming {old_filepath} to {new_filepath}")
-            # os.rename(old_filepath, new_filepath)
+            print(f"Renaming file: {file} to {file_snakecase}..")
+            os.rename(old_filepath, new_filepath)
 
         for subfolder in subfolders:
+            if subfolder == ".git":
+                continue
             subfolder_snakecase = subfolder.replace(" ", "_")
             old_path = os.path.join(folder, subfolder)
             new_path = os.path.join(folder, subfolder_snakecase)
 
-            print(f"renaming {old_path} to {new_path}")
-            # os.rename(old_path, new_path)
+            print(f"Renaming folder:{subfolder} to {subfolder_snakecase}...")
+            os.rename(old_path, new_path)
 
 
 rename_snake_case(os.getcwd())
