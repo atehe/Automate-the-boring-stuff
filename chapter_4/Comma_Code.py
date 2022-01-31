d6af252240de9a5439971491400d59ed0784407a
Stephen Atehe <atehestephen@gmail.com>
2022-01-31 20:54:47 +0100

    code fix for empty and single list

--- chapter_4/Comma_Code.py
+++ chapter_4/Comma_Code.py
@@ -1,9 +1,9 @@
 def comma_code(word_list):
     # handle empty list or single element list
     if len(word_list) == 0:
-        return word_list[0]
-    elif len(word_list) == 1:
         return ""
+    elif len(word_list) == 1:
+        return word_list[0]
 
     word_end = " and " + word_list[-1]
     word_start = ", ".join(word_list[:-1])
