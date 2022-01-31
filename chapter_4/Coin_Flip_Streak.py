18e0577faaebd9fce77aa7d2ac2db1cf6b49c017
Stephen Atehe <atehestephen@gmail.com>
2022-01-31 21:16:04 +0100

    complete code

--- chapter_4/Coin_Flip_Streak.py
+++ chapter_4/Coin_Flip_Streak.py
@@ -4,21 +4,19 @@ from typing import Text
 numberOfStreaks = 0
 
 for experimentNumber in range(1000):
-    #code that creates a list of 100 heads  or tails values
+    # code that creates a list of 100 heads  or tails values
     test_result = []
     for i in range(100):
         if random.randint(0, 1):
-            test_result.append('H')
+            test_result.append("H")
         else:
-            test_result.append('T')
-        print(test_result)
-    #code that checks if there is a streak of 6nheads or tails in a row
-    for i in range(len(test_result)):
-        
+            test_result.append("T")
 
-print(numberOfStreaks)
-                
-        
+    # code that checks if there is a streak of 6nheads or tails in a row
+    for i, flip in enumerate(test_result):
+        if i + 6 == len(test_result):
+            break
+        elif test_result[i : i + 6] == ["H"] * 6 or test_result[i : i + 6] == ["T"] * 6:
+            numberOfStreaks += 1
 
-
-print('Chances of streak: %s%%' % (numberOfStreaks / 100))
+print("Chances of streak: %s%%" % (numberOfStreaks / 100))
