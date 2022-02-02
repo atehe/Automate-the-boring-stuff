72f1d224c6b1277e32746cea2bf6752e2d72b2ae
Stephen Atehe <atehestephen@gmail.com>
2022-02-02 20:35:37 +0100

    refactor for readability

--- makedirs.py
+++ makedirs.py
@@ -6,6 +6,6 @@ cwd = Path.cwd()
 for i in range(15):
     folders = os.listdir(cwd)
     new_folder = "chapter_" + str(i)
-    if not new_folder in folders:
+    if new_folder not in folders:
         print(f"Creating {cwd }/{new_folder}")
         os.makedirs(cwd / new_folder)
